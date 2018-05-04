d96ea9a32e056b7140f508c243ca2c30c2dece89
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-05-04 15:37:33 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup_args= dict(
+    name = 'jupyterhub-ldap-authenticator',
+    version = '0.1.0',
+    description = 'LDAP Authenticator for JupyterHub',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords = ['Interactive', 'Interpreter', 'Shell', 'Web'],
+    url = 'https://github.com/hansohn/jupyterhub-ldap-authenticator',
+    author ='Ryan Hansohn',
+    license = '',
+    packages = ['ldapauthenticator'],
+    install_requires= [ 
+        'ldap3',
+        'jupyterhub'
+        'traitlets'
+    ]
+)
+
+#------------------------------------------------------------------------------
+# setup
+#------------------------------------------------------------------------------
+
+def main():
+    setup(**setup_args)
+
+if __name__ == '__main__':
+    main()
